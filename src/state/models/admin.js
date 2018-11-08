00e5e2073700d5bc0b43bf432549bc2704273065
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-11-07 22:39:03 -0800

    fixed redux action model axios calls for admin to point to new backend

--- src/state/models/admin.js
+++ src/state/models/admin.js
@@ -4,91 +4,91 @@ import Auth from './auth';
 class Admin {
   static getMostUsedSupplies = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminSupplies/mostUsed/${shop_id}`);
+    const response = await request(`/admin_supplies/most_used/${shop_id}`);
     return response.data.data;
   };
 
   static getPurchaseHistory = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminPurchases/purchaseHistory/${shop_id}`);
+    const response = await request(`/admin_purchases/purchase_history/${shop_id}`);
     return response.data.data;
   };
 
   static getMostOrderedSupplies = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminSupplies/mostOrdered/${shop_id}`);
+    const response = await request(`/admin_supplies/most_ordered/${shop_id}`);
     return response.data.data;
   };
 
   static getCurrentStaff = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminStaff/currentWorkingStaff/${shop_id}`);
+    const response = await request(`/admin_staff/current_working_staff/${shop_id}`);
     return response.data.data;
   };
 
   static getPurchaseStatuses = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminPurchases/purchasesStatus/${shop_id}`);
+    const response = await request(`/admin_purchases/purchases_status/${shop_id}`);
     return response.data.data;
   };
 
   static getCurrentStaffActivity = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminStaff/currentStaff/${shop_id}`);
+    const response = await request(`/admin_staff/current_staff/${shop_id}`);
     return response.data.data;
   };
 
   static getTotalProductsSold = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminProducts/totalProductSold/${shop_id}`);
+    const response = await request(`/admin_products/total_product_sold/${shop_id}`);
     return response.data.data;
   };
 
   static getNewPurchases = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminPurchases/newPurchases/${shop_id}`);
+    const response = await request(`/admin_purchases/new_purchases/${shop_id}`);
     return response.data.data;
   };
 
   static getTotalStaff = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminStaff/totalStaff/${shop_id}`);
+    const response = await request(`/admin_staff/total_staff/${shop_id}`);
     return response.data.data;
   };
 
   static getCompletedStaff = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminStaff/pastStaff/${shop_id}`);
+    const response = await request(`/admin_staff/past_staff/${shop_id}`);
     return response.data.data;
   };
 
   static getTotalBundlesSold = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminProducts/totalBundleSold/${shop_id}`);
+    const response = await request(`/admin_products/total_bundle_sold/${shop_id}`);
     return response.data.data;
   };
 
   static getTotalItemsSold = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminProducts/totalItemSold/${shop_id}`);
+    const response = await request(`/admin_products/total_item_sold/${shop_id}`);
     return response.data.data;
   };
 
   static getCompletedPurchases = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminPurchases/completedPurchases/${shop_id}`);
+    const response = await request(`/admin_purchases/completed_purchases/${shop_id}`);
     return response.data.data;
   };
 
   static getInProductionPurchases = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminPurchases/productionPurchases/${shop_id}`);
+    const response = await request(`/admin_purchases/production_purchases/${shop_id}`);
     return response.data.data;
   };
 
   static getTotalItemsSoldChart = async () => {
     const shop_id = await Auth._authenticatedRequest();
-    const response = await request(`/adminProducts/itemQTY/${shop_id}`);
+    const response = await request(`/admin_products/item_qty/${shop_id}`);
     return response.data.data;
   };
 
